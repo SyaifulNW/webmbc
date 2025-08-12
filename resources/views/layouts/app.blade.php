b6bd9d3ec7236cdd8932e8d7685020ea5beafa12
Syaiful Nur Wardani <79695832+SyaifulNW@users.noreply.github.com>
2025-08-12 09:27:25 +0700

    cek

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-<link href="https://e9bd592fef32.ngrok-free.app/backend/css/sb-admin-2.min.css" rel="stylesheet">
+<link href="https://98231776859c.ngrok-free.app/backend/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="{{asset('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 </head>
